88a332c21b8d582c81711e95a3135dff9ee30638
ntwari <ntwariegide2@gmail.com>
2021-08-19 20:22:58 +0200

    add redux in component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Select, { components } from "react-select";
 import "react-multi-carousel/lib/styles.css";
 import Carousel from "react-multi-carousel";
 import {useState,useEffect} from "react"
+import axios from "axios"
 
 const options = [
   { value: "England", label: "England", icon: "https://www.countryflags.io/be/shiny/64.png" },
@@ -41,197 +42,29 @@ const IconOption = props => (
 
 function App() {
 
+  const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  }
+
+  const initialData = {
+    allcountriesdata: [],
+    countrydata: [],
+    error: null,
+  };
+
   const [allcontinentsdata,setallcontinentsdata] = useState()
 
-  const [allcountritesdata,setallcountritesdata] = useState([
-    {
-      "updated": 1587140875524,
-      "country": "Afghanistan",
-      "countryInfo": {
-        "_id": 4,
-        "iso2": "AF",
-        "iso3": "AFG",
-        "lat": 33,
-        "long": 65,
-        "flag": "https://raw.githubusercontent.com/NovelCOVID/API/master/assets/flags/af.png"
-      },
-      "cases": 840,
-      "todayCases": 56,
-      "deaths": 30,
-      "todayDeaths": 5,
-      "recovered": 54,
-      "active": 756,
-      "critical": 0,
-      "casesPerOneMillion": 22,
-      "deathsPerOneMillion": 0,
-      "tests": 0,
-      "testsPerOneMillion": 0
-    },
-    {
-      "updated": 1587140875533,
-      "country": "Albania",
-      "countryInfo": {
-        "_id": 8,
-        "iso2": "AL",
-        "iso3": "ALB",
-        "lat": 41,
-        "long": 20,
-        "flag": "https://raw.githubusercontent.com/NovelCOVID/API/master/assets/flags/al.png"
-      },
-      "cases": 518,
-      "todayCases": 24,
-      "deaths": 26,
-      "todayDeaths": 1,
-      "recovered": 277,
-      "active": 215,
-      "critical": 5,
-      "casesPerOneMillion": 180,
-      "deathsPerOneMillion": 9,
-      "tests": 4827,
-      "testsPerOneMillion": 1677
-    },
-    {
-      "updated": 1587140875510,
-      "country": "Algeria",
-      "countryInfo": {
-        "_id": 12,
-        "iso2": "DZ",
-        "iso3": "DZA",
-        "lat": 28,
-        "long": 3,
-        "flag": "https://raw.githubusercontent.com/NovelCOVID/API/master/assets/flags/dz.png"
-      },
-      "cases": 2268,
-      "todayCases": 108,
-      "deaths": 348,
-      "todayDeaths": 12,
-      "recovered": 783,
-      "active": 1137,
-      "critical": 60,
-      "casesPerOneMillion": 52,
-      "deathsPerOneMillion": 8,
-      "tests": 3359,
-      "testsPerOneMillion": 77
-    },
-    {
-      "updated": 1587140875529,
-      "country": "Andorra",
-      "countryInfo": {
-        "_id": 20,
-        "iso2": "AD",
-        "iso3": "AND",
-        "lat": 42.5,
-        "long": 1.6,
-        "flag": "https://raw.githubusercontent.com/NovelCOVID/API/master/assets/flags/ad.png"
-      },
-      "cases": 673,
-      "todayCases": 0,
-      "deaths": 33,
-      "todayDeaths": 0,
-      "recovered": 169,
-      "active": 471,
-      "critical": 17,
-      "casesPerOneMillion": 8710,
-      "deathsPerOneMillion": 427,
-      "tests": 1673,
-      "testsPerOneMillion": 21653
-    },
-    {
-      "updated": 1587140875574,
-      "country": "Angola",
-      "countryInfo": {
-        "_id": 24,
-        "iso2": "AO",
-        "iso3": "AGO",
-        "lat": -12.5,
-        "long": 18.5,
-        "flag": "https://raw.githubusercontent.com/NovelCOVID/API/master/assets/flags/ao.png"
-      },
-      "cases": 19,
-      "todayCases": 0,
-      "deaths": 2,
-      "todayDeaths": 0,
-      "recovered": 5,
-      "active": 12,
-      "critical": 0,
-      "casesPerOneMillion": 0,
-      "deathsPerOneMillion": 0,
-      "tests": 0,
-      "testsPerOneMillion": 0
-    },
-    {
-      "updated": 1587140875596,
-      "country": "Anguilla",
-      "countryInfo": {
-        "_id": 660,
-        "iso2": "AI",
-        "iso3": "AIA",
-        "lat": 18.25,
-        "long": -63.1667,
-        "flag": "https://raw.githubusercontent.com/NovelCOVID/API/master/assets/flags/ai.png"
-      },
-      "cases": 3,
-      "todayCases": 0,
-      "deaths": 0,
-      "todayDeaths": 0,
-      "recovered": 1,
-      "active": 2,
-      "critical": 0,
-      "casesPerOneMillion": 200,
-      "deathsPerOneMillion": 0,
-      "tests": 0,
-      "testsPerOneMillion": 0
-    },
-    {
-      "updated": 1587140875572,
-      "country": "Antigua and Barbuda",
-      "countryInfo": {
-        "_id": 28,
-        "iso2": "AG",
-        "iso3": "ATG",
-        "lat": 17.05,
-        "long": -61.8,
-        "flag": "https://raw.githubusercontent.com/NovelCOVID/API/master/assets/flags/ag.png"
-      },
-      "cases": 23,
-      "todayCases": 0,
-      "deaths": 3,
-      "todayDeaths": 1,
-      "recovered": 3,
-      "active": 17,
-      "critical": 1,
-      "casesPerOneMillion": 235,
-      "deathsPerOneMillion": 31,
-      "tests": 73,
-      "testsPerOneMillion": 745
-    },
-    {
-      "updated": 1587140875508,
-      "country": "Argentina",
-      "countryInfo": {
-        "_id": 32,
-        "iso2": "AR",
-        "iso3": "ARG",
-        "lat": -34,
-        "long": -64,
-        "flag": "https://raw.githubusercontent.com/NovelCOVID/API/master/assets/flags/ar.png"
-      },
-      "cases": 2669,
-      "todayCases": 98,
-      "deaths": 122,
-      "todayDeaths": 10,
-      "recovered": 631,
-      "active": 1916,
-      "critical": 121,
-      "casesPerOneMillion": 59,
-      "deathsPerOneMillion": 3,
-      "tests": 26457,
-      "testsPerOneMillion": 585
-    }
-  ])
+  const [allcountritesdata,setallcountritesdata] = useState([])
+
+  const [data, dispatch] = React.useReducer(dataReducer, initialData);
 
   useEffect(()=>{
-      setallcountritesdata()
+
+      axios.get(`https://corona.lmao.ninja/v2/countries?yesterday&sort`,headers)
+      .then( response => {
+        dispatch({ type: "SET_ALL_COUNTRIES",allcountritesdata: response.data})
+      })
 
       allcountritesdata.map(countrydata => {
           options.push({ value: countrydata.country, label: countrydata.country, icon: `https://www.countryflags.io/be/${countrydata.country.toLowerCase}/64.png` })
