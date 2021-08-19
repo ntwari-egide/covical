95f859b24a47c69b96c86ae28487f8707177413a
ntwari <ntwariegide2@gmail.com>
2021-08-19 03:27:03 +0200

    fix bug in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./navbar.css"
+import LogoImage from "../assets/images/C O V I C A L C.png"
+
 /**
  * 
  */
@@ -10,13 +12,13 @@ export default function Navbar({ fixed }) {
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-pink-500 mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <h1 className="logo-name"></h1>
+            <img className="logo-img" src={LogoImage} />
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}
             >
-              <i className="fas fa-bars"></i>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M3 4h18v2H3V4zm0 7h18v2H3v-2zm0 7h18v2H3v-2z" fill="rgba(254,254,254,1)"/></svg>
             </button>
           </div>
           <div
@@ -26,7 +28,13 @@ export default function Navbar({ fixed }) {
             }
             id="example-navbar-danger"
           >
-            <button className="contact-button text-sm text-white font-bold center">CONTACT</button>
+
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="nav-item">
+                <button className="contact-button text-sm text-white font-bold center">CONTACT</button>
+              </li>
+            </ul>
+
 
           </div>
         </div>
