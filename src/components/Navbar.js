be60546b6db53781e55fa434eb787830b1e28ab5
ntwari <ntwariegide2@gmail.com>
2021-08-20 23:33:24 +0200

    add JSDOC comments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,78 @@
 import React from "react";
+
 import "./navbar.css"
+
 import LogoImage from "../assets/images/C O V I C A L C.png"
 
 /**
- * 
- */
+* @description Navbar component
+*/
+
 export default function Navbar({ fixed }) {
+
+  /**
+  * @description setting required states to be used in this Navbar component
+  */
+
   const [navbarOpen, setNavbarOpen] = React.useState(false);
+
   return (
+    
     <>
+    
       <nav className="navigation-bar relative flex flex-wrap items-center justify-between px-2 py-3">
+    
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+    
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+    
             <img className="logo-img" alt="" src={LogoImage} />
+    
             <button
+    
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              
               type="button"
+              
               onClick={() => setNavbarOpen(!navbarOpen)}
+              
             >
+            
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M3 4h18v2H3V4zm0 7h18v2H3v-2zm0 7h18v2H3v-2z" fill="rgba(254,254,254,1)"/></svg>
+            
             </button>
+        
           </div>
+        
           <div
+        
             className={
               "lg:flex flex-grow items-center" +
               (navbarOpen ? " flex" : " hidden")
+        
             }
+        
             id="example-navbar-danger"
           >
 
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+            
               <li className="nav-item">
+            
                 <button className="contact-button text-sm text-white font-bold center">CONTACT</button>
+            
               </li>
+            
             </ul>
 
           </div>
+ 
         </div>
+ 
       </nav>
+ 
     </>
-  );
+ 
+ );
+
 }
